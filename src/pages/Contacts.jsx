7c914dab89dfaa13bb7487dc245a8c8bdf31dfe4
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-03-31 01:26:27 +0530

    Add update functionality to Contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,6 +5,7 @@ import femaleImg from '../assets/female.png';
 import useContactAPI from '../hooks/useContactAPI';
 import pencil from '../assets/pencil.svg';
 import trash from '../assets/trash.svg';
+import change from '../assets/change.svg';
 
 const Contacts = () => {
   const {
@@ -16,10 +17,111 @@ const Contacts = () => {
     updateContact,
   } = useContactAPI();
 
+  const [index, setIndex] = useState(null);
+  const [selectedContact, setSelectedContact] = useState({});
+  const [updateContactData, setUpdateContactData] = useState({});
+
+  const toggleGender = () => {
+    setSelectedContact((prevState) => ({
+      ...prevState,
+      gender:
+        prevState.gender === 'male'
+          ? 'female'
+          : prevState.gender === 'female'
+          ? 'male'
+          : undefined,
+    }));
+    setUpdateContactData((prevState) => ({
+      ...prevState,
+      gender: selectedContact.gender === 'male' ? 'female' : 'male',
+    }));
+  };
+
+  const changeData = (e) => {
+    setUpdateContactData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+
+    setSelectedContact((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   useEffect(() => {
     getContacts();
   }, []);
 
+  const updateInputBoxes = [
+    {
+      id: 1,
+      component: (
+        <div className="flex flex-row justify-center items-center bg-[#e7eced] py-2 px-2  h-[3rem] max-w-fit gap-1">
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={selectedContact ? selectedContact.fullName : ''}
+            onChange={(e) => changeData(e)}
+            className="bg-[#e7eced] font-['poppins'] text-[20px] leading-[50px] font-medium   text-[#083F46]  focus:outline-none max-w-[220px]"
+          />
+          <div className="border-r-2 border-[#083F46] h-[2rem]"></div>
+        </div>
+      ),
+    },
+    {
+      id: 2,
+      component: (
+        <div className="flex flex-row justify-center items-center bg-[#e7eced] py-2 px-2  h-[3rem] max-w-fit  gap-1">
+          <input
+            disabled={true}
+            type="text"
+            id="gender"
+            name="gender"
+            value={selectedContact ? selectedContact.gender : ''}
+            className="bg-[#e7eced] font-['poppins'] text-[20px] leading-[50px] font-medium   text-[#083F46]  focus:outline-none max-w-[80px]"
+          />
+          <img src={change} className="w-5" onClick={() => toggleGender()} />
+        </div>
+      ),
+    },
+    {
+      id: 3,
+      component: (
+        <div className="flex flex-row justify-center items-center bg-[#e7eced] py-2 px-2  h-[3rem]  max-w-fit gap-1 ">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={selectedContact ? selectedContact.email : ''}
+            onChange={(e) => changeData(e)}
+            className="bg-[#e7eced] font-['poppins'] text-[20px] leading-[50px] font-medium   text-[#083F46]  focus:outline-none max-w-[330px]"
+          />
+          <div className="border-r-2 border-[#083F46] h-[2rem]"></div>
+        </div>
+      ),
+    },
+    {
+      id: 4,
+      component: (
+        <div className="flex flex-row justify-center items-center bg-[#e7eced] py-2 px-2  h-[3rem] max-w-fit  gap-1 ">
+          <input
+            type="text"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={selectedContact ? selectedContact.phoneNumber : ''}
+            onChange={(e) => changeData(e)}
+            className="bg-[#e7eced] font-['poppins'] text-[20px] leading-[50px] font-medium   text-[#083F46]  focus:outline-none max-w-[150px]"
+          />
+          <div className="border-r-2 border-[#083F46] h-[2rem]"></div>
+        </div>
+      ),
+    },
+  ];
+
+  console.log('updateContactData  ', updateContactData);
+  console.log('selectedContact', selectedContact);
   return (
     <>
       <div className="container max-w-screen-xl text-white mb-10">
@@ -65,29 +167,73 @@ const Contacts = () => {
                 </tr>
               </thead>
               <tbody>
-                {contacts.map((contact) => {
+                {contacts.map((contact, idx) => {
                   return (
                     <tr
                       key={contact._id}
                       className="bg-white border-b text-[20px] leading-[50px] font-['poppins'] "
                     >
-                      <td className="px-2 py-4 ">
+                      <td className="px-2 py-4 min-w-[105px]">
                         <img
                           src={contact.gender === 'male' ? maleImg : femaleImg}
                           className="w-[60px] h-[60px] rounded-full"
                         />
                       </td>
-                      <td className="px-3 py-4">{contact.fullName}</td>
-                      <td className="px-3 py-4">{contact.gender}</td>
-                      <td className="px-3 py-4">{contact.email}</td>
-                      <td className="px-3 py-4">{contact.phoneNumber}</td>
-                      <td className="px-3 py-4  ">
-                        <button>
-                          <img src={pencil} className="w-5 mr-5 " />
-                        </button>
-                        <button>
-                          <img src={trash} className="w-5  " />
-                        </button>
+                      <td className="px-3 py-4 min-w-[262px]">
+                        {index === idx
+                          ? updateInputBoxes[0].component
+                          : contact.fullName}
+                      </td>
+                      <td className="px-3 py-4 min-w-[118px]">
+                        {index === idx
+                          ? updateInputBoxes[1].component
+                          : contact.gender}
+                      </td>
+                      <td className="px-3 py-4 min-w-[330px]">
+                        {index === idx
+                          ? updateInputBoxes[2].component
+                          : contact.email}
+                      </td>
+                      <td className="px-3 py-4 min-w-[200px]">
+                        {index === idx
+                          ? updateInputBoxes[3].component
+                          : contact.phoneNumber}
+                      </td>
+                      <td className="px-3 py-4 min-w-[100px] ">
+                        {index === idx ? (
+                          <div className="flex flex-row gap-2">
+                            <button
+                              onClick={() => {
+                                setIndex(null);
+                                setUpdateContactData({});
+                                setSelectedContact({});
+                              }}
+                              className="py-1 bg-[#fd6262] text-white text-sm rounded-2xl px-3"
+                            >
+                              {' '}
+                              x{' '}
+                            </button>
+                            <button className="p-2 bg-[#083F46] text-white text-sm rounded-2xl">
+                              save
+                            </button>
+                          </div>
+                        ) : (
+                          <>
+                            <button>
+                              <img
+                                src={pencil}
+                                className="w-5 mr-5 "
+                                onClick={() => (
+                                  setIndex(idx),
+                                  setSelectedContact({ ...contact })
+                                )}
+                              />
+                            </button>
+                            <button>
+                              <img src={trash} className="w-5  " />
+                            </button>
+                          </>
+                        )}
                       </td>
                     </tr>
                   );
